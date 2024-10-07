8e9e58c9ba4780bc4b28d34198eea9a78fc597d2
ShamiyaParvin <shamiyaparvin547@gmail.com>
2024-10-07 11:09:22 +0530

    initial commit

---
+++ c-sharp-2509-1/c-sharp-2509-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_2509_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            while (number != 0)
+            {
+                number /= 10;
+                count++;
+            }
+            Console.WriteLine("Number of digits: " + count);
+            Console.ReadLine();
+        }
+    }
+}
